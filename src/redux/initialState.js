74a305cae5f22b2df0d7e032553d5a8500541792
Katarzyna Nizioł <niziol.katarzyna25@gmail.com>
2023-12-08 00:24:55 +0100

    add subreducer

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -15,37 +15,47 @@ const initialState = {
   columns: [
     {
       listId: '1',
-      id: 1,
+      id: '1',
       title: 'Books',
       icon: 'book',
     },
     {
       listId: '1',
-      id: 2,
+      id: '2',
       title: 'Movies',
       icon: 'film',
     },
     {
       listId: '1',
-      id: 3,
+      id: '3',
       title: 'Games',
       icon: 'gamepad',
     },
     {
       listId: '2',
-      id: 4,
+      id: '4',
       title: 'Play',
       icon: 'play',
     },
   ],
 
   cards: [
-    { id: 1, columnId: 1, title: 'This is Going to Hurt', isFavorite: true },
-    { id: 2, columnId: 1, title: 'Interpreter of Maladies', isFavorite: false },
-    { id: 3, columnId: 2, title: 'Harry Potter', isFavorite: false },
-    { id: 4, columnId: 2, title: 'Star Wars', isFavorite: true },
-    { id: 5, columnId: 3, title: 'The Witcher', isFavorite: false },
-    { id: 6, columnId: 3, title: 'Skyrim', isFavorite: false },
+    {
+      id: '1',
+      columnId: '1',
+      title: 'This is Going to Hurt',
+      isFavorite: true,
+    },
+    {
+      id: '2',
+      columnId: '1',
+      title: 'Interpreter of Maladies',
+      isFavorite: false,
+    },
+    { id: '3', columnId: '2', title: 'Harry Potter', isFavorite: false },
+    { id: '4', columnId: '2', title: 'Star Wars', isFavorite: true },
+    { id: '5', columnId: '3', title: 'The Witcher', isFavorite: false },
+    { id: '6', columnId: '3', title: 'Skyrim', isFavorite: false },
   ],
 
   searchString: '',
--- src/redux/store.js
+++ src/redux/store.js
@@ -38,43 +38,60 @@ export const toggleCardFavorite = (payload) => ({
   payload,
 });
 
-const reducer = (state, action) => {
+const listsReducer = (statePart = [], action) => {
+  switch (action.type) {
+    case 'ADD_LIST':
+      return [...statePart, { ...action.payload, id: shortid() }];
+    default:
+      return statePart;
+  }
+};
+
+const columnsReducer = (statePart = [], action) => {
   switch (action.type) {
     case 'ADD_COLUMN':
-      return {
-        ...state,
-        columns: [...state.columns, { ...action.payload, id: shortid() }],
-      };
-    case 'ADD_CARD':
-      return {
-        ...state,
-        cards: [
-          ...state.cards,
-          { ...action.payload, id: shortid(), isFavorite: false },
-        ],
-      };
-    case 'UPDATE_SEARCHSTRING':
-      return {
-        ...state,
-        searchString: action.payload.value,
-      };
-    case 'ADD_LIST':
-      return {
-        ...state,
-        lists: [...state.lists, { ...action.payload, id: shortid() }],
-      };
-    case 'TOGGLE_CARD_FAVORITE':
-      return {
-        ...state,
-        cards: state.cards.map((card) =>
-          card.id === action.payload
-            ? { ...card, isFavorite: !card.isFavorite }
-            : card
-        ),
-      };
+      return [...statePart, { ...action.payload, id: shortid() }];
     default:
-      return state;
+      return statePart;
   }
+};
+
+const cardsReducer = (statePart = [], action) => {
+  switch (action.type) {
+    case 'ADD_CARD':
+      return [
+        ...statePart,
+        { ...action.payload, id: shortid(), isFavorite: false },
+      ];
+    case 'TOGGLE_CARD_FAVORITE':
+      return statePart.map((card) =>
+        card.id === action.payload
+          ? { ...card, isFavorite: !card.isFavorite }
+          : card
+      );
+    default:
+      return statePart;
+  }
+};
+
+const searchStringReducer = (statePart = '', action) => {
+  switch (action.type) {
+    case 'UPDATE_SEARCHSTRING':
+      return action.payload.value;
+    default:
+      return statePart;
+  }
+};
+
+const reducer = (state, action) => {
+  const newState = {
+    lists: listsReducer(state.lists, action),
+    columns: columnsReducer(state.columns, action),
+    cards: cardsReducer(state.cards, action),
+    searchString: searchStringReducer(state.searchString, action),
+  };
+
+  return newState;
 };
 const store = createStore(
   reducer,
